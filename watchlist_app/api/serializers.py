50e812662110d4f94e14cdb8a632b5f24b94d193
VikasSharma <vikas1.sharma@trantorinc.com>
2023-10-16 20:15:13 +0530

    using ModelSerializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,18 +1,7 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id =serializers.IntegerField(read_only=True)
-    name =serializers.CharField()
-    descriptions =serializers.CharField()
-    active =serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.descriptions = validated_data.get('descriptions', instance.descriptions)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
