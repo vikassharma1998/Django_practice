20300287a41f20e5aa263804eeabc6ef2c5a32be
VikasSharma <vikas1.sharma@trantorinc.com>
2023-10-16 22:41:06 +0530

    using custom serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,10 +2,18 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    label = serializers.SerializerMethodField()
+    key = serializers.SerializerMethodField()
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['label', 'key',]
 
+    def get_label(self, obj):
+        return obj.name
+    
+    def get_key(self, obj):
+        return obj.pk
+    
     def validate_name(self, value):
         if len(value) < 4:
             raise serializers.ValidationError("Movie name must greater than 5 characters")
